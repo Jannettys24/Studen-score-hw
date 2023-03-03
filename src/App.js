ed3994a5f98d58a67383ede6dca979d3ce5a0cf0
Jannettys24 <118224718+Jannettys24@users.noreply.github.com>
2023-03-03 15:03:34 -0500

    student score

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import userData from './data';
+import StudentCard from './component/StudentCard';
 
 function App() {
+  const [user,setUser] = useState(userData)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Student Data</h1>
+
+     <StudentCard user={user} />
+     
+     
+     
     </div>
   );
 }
